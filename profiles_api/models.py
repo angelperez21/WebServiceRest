ed89514d36b5ac66224dd67bd049d39508cf48e7
Ángel Pérez <dev.angel.p@gmail.com>
2020-11-11 13:53:04 -0600

    Bienvenido al Ejemplo de Web Service REST con el framework Django de python

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManagerU(BaseUserManager):
+    """ Manager para perfiles de usuario """
+
+    #Con esta función permitiremos la creación de usuario desde linea de comandos
+    def create_user(self, email, name, passwd=None):
+        ''' Creación de nuevo user profile '''
+        if not email:
+            raise ValueError('El usuario debe tener un Email')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(passwd)
+        user.save(using=self.db)
+        return user
 
 class UserProfile (AbstractUser,PermissionsMixin):
     """ Modelo Base de Datos para usarios en el sistema """
@@ -22,4 +39,6 @@ class UserProfile (AbstractUser,PermissionsMixin):
         '''Obtener el nombre corto del usuario'''
         return self.name
 
-    def __str__ 
+    def __str__(self):
+        ''' Regresamos una cadena de representación de nuestro usuario'''
+        return self.email   
